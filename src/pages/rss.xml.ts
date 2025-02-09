f37304a7cdcb1197f72bc7cd10b37590ca1bf099
Mustafa Ozaytac <ozaytac@gmail.com>
2025-02-09 21:20:42 +0300

    fix1

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,14 +3,16 @@ import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 import sanitizeHtml from 'sanitize-html';
 import MarkdownIt from 'markdown-it';
+import type { APIContext } from 'astro';
+
 const parser = new MarkdownIt();
 
-export async function GET(context) {
+export async function GET(context: APIContext) {
   const posts = await getCollection('blog');
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
-    site: context.site,
+    site: context.site!,
     items: posts.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
